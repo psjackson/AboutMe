66b817397ee8c5708f0f992701c393c95082e8c0
Peter Jackson <Peter@Peters-MacBook-Pro.local>
2018-01-20 18:09:11 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,16 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Version one.
+        String my_name = "Pete Jackson";
+        Integer my_age = 39;
+        String like1 = "Soccer";
+        String like2 = "Pandas";
+        String like3 = "Board Games";
+        System.out.println("Hi, my name is " + my_name);
+        System.out.println("I'm " + my_age + " years old.");
+        System.out.println("I like " + like1 + ", " + like2 + " and " + like3 + ".");
+    }
+}
