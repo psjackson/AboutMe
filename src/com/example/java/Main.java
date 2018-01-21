dc124f267cf204348d949e3100507f0309b581ff
Peter Jackson <pjackson@students.mchenry.edu>
2018-01-20 18:20:01 -0600

    Updating variables

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -3,12 +3,12 @@ package com.example.java;
 public class Main {
 
     public static void main(String[] args) {
-        //Version one.
+        //Version two.
         String my_name = "Pete Jackson";
         Integer my_age = 39;
-        String like1 = "Soccer";
-        String like2 = "Pandas";
-        String like3 = "Board Games";
+        String like1 = "soccer";
+        String like2 = "pandas";
+        String like3 = "all kinds of boardgames";
         System.out.println("Hi, my name is " + my_name);
         System.out.println("I'm " + my_age + " years old.");
         System.out.println("I like " + like1 + ", " + like2 + " and " + like3 + ".");
